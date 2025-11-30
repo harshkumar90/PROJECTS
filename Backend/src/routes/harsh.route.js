2129cad55bbe2f140cc24c9ba3767388841e18eb
harshkumar90 <harshkumar91426@gmail.com>
2025-11-30 23:46:08 +0530

    Delete Backend directory

--- Backend/src/routes/harsh.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const router=express.Router();
-
-router.get("/harsh",(req,res)=>{
-    res.send("harsh is a good boy")
-})
-export default router;
--- Backend/src/routes/messages.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const router=express.Router();
-
-router.get("/send",(req,res)=>{
-    res.send("send message to end point")
-})
-export default router;
